c975cf8d41b3d3b0b59d819283c83b283ca5f8db
Andrew Smith <andrew@crowdlinker.com>
2017-12-06 18:49:08 -0700

    updated w/ context provider

--- examples/exampleApp/App.js
+++ examples/exampleApp/App.js
@@ -1,37 +1,20 @@
 import React from "react";
 import { StyleSheet, Text, View } from "react-native";
-import { AppState, NetInfo, Geolocation } from "react-native-fns";
+import { Provider, Connected } from "react-native-fns";
 export default class App extends React.Component {
+
   render() {
     return (
-      <View style={styles.container}>
-        <AppState
-          render={({ appState }) => {
-            return (
-              <NetInfo
-                render={({ connectionInfo }) => {
-                  return (
-                    <Geolocation
-                      render={({ position, positionError }) => {
-                        return (
-                          <View>
-                            <Text>
-                              NetInfo: {`${JSON.stringify(connectionInfo)}`}
-                            </Text>
-                            <Text>AppState: {`${appState}`} </Text>
-                            <Text>Geolocation: {`${JSON.stringify(position)} ${positionError}`}</Text>
-                          </View>
-                        );
-                      }}
-                    />
-                  );
-                }}
-              />
-            );
-          }}
-        />
-      </View>
-    );
+      <Provider>
+        <View>
+            <Connected render={(states) => (
+              <Text>
+                {`${JSON.stringify(states, 2, '\t')}`}
+                </Text>
+            )} />
+          </View>
+      </Provider>
+    )
   }
 }
 
